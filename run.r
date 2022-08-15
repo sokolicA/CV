1b1a6c216a38907c1c6824f56409d8c5faf1a066
sokolicA <andrejsokolicc@gmail.com>
2022-08-15 17:24:05 +0200

    renamed files and folders

--- run.r
+++ run.r
@@ -10,19 +10,19 @@
 
 
 # Knit the HTML version
-rmarkdown::render(here::here("02_Report", "cv_en.rmd"),
+rmarkdown::render(here::here("02_report", "cv_en.rmd"),
                   params = list(pdf_mode = FALSE),
-                  output_file = "cv_en.html")
+                  output_file = "andrej_sokolic_cv_en.html")
 
 # Knit the PDF version to temporary html location
 tmp_html_cv_en_loc <- fs::file_temp(ext = ".html")
-rmarkdown::render(here::here("02_Report", "cv_en.rmd"),
+rmarkdown::render(here::here("02_report", "cv_en.rmd"),
                   params = list(pdf_mode = TRUE),
                   output_file = tmp_html_cv_en_loc)
 
 # Convert to PDF using Pagedown
 pagedown::chrome_print(input = tmp_html_cv_en_loc,
-                       output = here::here("02_Report", "cv_en.pdf")
+                       output = here::here("02_report", "andrej_sokolic_cv_en.pdf")
 )
 
 
@@ -30,17 +30,17 @@ pagedown::chrome_print(input = tmp_html_cv_en_loc,
 # Slovenian ---------------------------------------------------------------
 
 # Knit the HTML version
-rmarkdown::render(here::here("02_Report", "cv_slo.rmd"),
+rmarkdown::render(here::here("02_report", "cv_slo.rmd"),
                   params = list(pdf_mode = FALSE),
-                  output_file = "cv_slo.html")
+                  output_file = "andrej_sokolic_cv_slo.html")
 
 # Knit the PDF version to temporary html location
 tmp_html_cv_slo_loc <- fs::file_temp(ext = ".html")
-rmarkdown::render(here::here("02_Report", "cv_slo.rmd"),
+rmarkdown::render(here::here("02_report", "cv_slo.rmd"),
                   params = list(pdf_mode = TRUE),
                   output_file = tmp_html_cv_slo_loc)
 
 # Convert to PDF using Pagedown
 pagedown::chrome_print(input = tmp_html_cv_slo_loc,
-                       output = here::here("02_Report", "cv_slo.pdf")
+                       output = here::here("02_report", "andrej_sokolic_cv_slo.pdf")
 )
